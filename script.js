f5731590a8a8896b4551604cf381139832019e06
BrenoHallan <brenohmt88@hotmail.com>
2024-01-29 18:14:15 -0300

    d

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let ok = 'ok'
-
 const html = document.querySelector('html')
 const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
